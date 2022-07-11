6cc3cbfbb7648aed2fb00fdc5f7f95693c8b9af9
bolunzhang <zhang_bolun@163.com>
2022-07-12 01:34:57 +0800

    second-branch modified4

--- src/main/java/com/bolunzhang/GitTest.java
+++ src/main/java/com/bolunzhang/GitTest.java
@@ -4,5 +4,19 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("success");
         System.out.println("success");
+        System.out.println("success");
+        System.out.println("success");
+        System.out.println("success");
+        System.out.println("success");
+        System.out.println("success");
+        System.out.println("success");
+        System.out.println("success");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+
     }
 }
